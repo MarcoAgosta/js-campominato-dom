8dd3e07e47a6659e19a1be92189cf766a655a098
MarcoAgosta <agostamarco@libero.it>
2022-11-10 16:07:58 +0100

    pusho fino a problema

--- JS/functions/listaMine.js
+++ JS/functions/listaMine.js
@@ -1,17 +1,16 @@
 /**La funzione restituisce una lista di numeri casuali da 1 fino al massimo indicato.
  * 
  */
-function listaMine (num){
+function listMine(numero) {
 
-    const numero = parseInt(num)
-    const arrayMine = [];
+    const num = parseInt(numero)
+    let arrayMine = [];
 
     while (arrayMine.length <= 16){
 
-       const numeroMina = Math.floor(Math.random() * numero +1);
+       const numeroMina = Math.floor(Math.random() * num +1);
 
        if (arrayMine.includes(numeroMina)){
-
        } else {
 
         arrayMine.push(numeroMina)
